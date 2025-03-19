57c17a862f7b14ee74d36b470b89ae49f0dc3d71
rodgers3333 <brandonrodgers3333@gmail.com>
2025-03-19 15:48:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const tablinks = document.getElementsByClassName("tab-links");
-const tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-    for(tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
 const sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
